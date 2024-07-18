bcac3a861a7b5f1af50c5ac00cba0447f7ea5d47
john Brandews <johnbrandews@gmail.com>
2024-07-18 23:03:46 +0300

    added file for read more

---
+++ single.php
@@ -0,0 +1,121 @@
+<?php include("path.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/8ef1de9f50.js" crossorigin="anonymous"></script>
+    <!--google fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Candal&family=Lora:ital@0;1&display=swap" rel="stylesheet">
+<!-- custom styling -->
+<link rel="stylesheet" href="assets/css/style.css">
+    <title>single post</title>
+    
+    <meta name="viewport"
+    content="width=device-width,initial-scale = 1.0">
+</head>
+<body>
+<?php include(ROOT_PATH."/apps/includes/header.php"); ?>
+
+    
+    <!--page wrapper-->
+    <div class="page-wrapper">
+    <!--content-->
+    <div class="content clearfix">
+        <!--main content-->
+        <div class="main-content single">
+<h1 class="post-title">This is the title of the post</h1>
+<div class="post-content">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+         Repellat illo praesentium quo debitis recusandae maiores.</p>
+         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam minima alias assumenda placeat maxime ipsum nobis, vel eaque. Velit inventore distinctio voluptates, dolor qui blanditiis eveniet.
+             Eos cupiditate consectetur quidem.</p>
+             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, quis. Recusandae, labore! Nemo accusamus placeat at officiis provident reprehenderit est quod odio blanditiis, beatae sapiente quo vero, facilis corporis deleniti nobis molestias tenetur illum quidem sit explicabo incidunt. 
+                Sit, autem?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                     Doloribus, labore.</p>
+                     
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Repellat illo praesentium quo debitis recusandae maiores.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam minima alias assumenda placeat maxime ipsum nobis, vel eaque. Velit inventore distinctio voluptates, dolor qui blanditiis eveniet.
+            Eos cupiditate consectetur quidem.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, quis. Recusandae, labore! Nemo accusamus placeat at officiis provident reprehenderit est quod odio blanditiis, beatae sapiente quo vero, facilis corporis deleniti nobis molestias tenetur illum quidem sit explicabo incidunt. 
+               Sit, autem?</p>
+               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Doloribus, labore.</p>
+                    
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Repellat illo praesentium quo debitis recusandae maiores.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam minima alias assumenda placeat maxime ipsum nobis, vel eaque. Velit inventore distinctio voluptates, dolor qui blanditiis eveniet.
+            Eos cupiditate consectetur quidem.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, quis. Recusandae, labore! Nemo accusamus placeat at officiis provident reprehenderit est quod odio blanditiis, beatae sapiente quo vero, facilis corporis deleniti nobis molestias tenetur illum quidem sit explicabo incidunt. 
+               Sit, autem?</p>
+               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Doloribus, labore.</p>
+                    
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Repellat illo praesentium quo debitis recusandae maiores.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam minima alias assumenda placeat maxime ipsum nobis, vel eaque. Velit inventore distinctio voluptates, dolor qui blanditiis eveniet.
+            Eos cupiditate consectetur quidem.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, quis. Recusandae, labore! Nemo accusamus placeat at officiis provident reprehenderit est quod odio blanditiis, beatae sapiente quo vero, facilis corporis deleniti nobis molestias tenetur illum quidem sit explicabo incidunt. 
+               Sit, autem?</p>
+               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Doloribus, labore.</p>
+</div>
+        </div>
+        <div class="sidebar single">
+            <div class="section popular">
+                <h2 class="section-title">popular</h2>
+                <div class="post clearfix">
+                    <img src="assets/images/20230923_085235.jpg" alt="" >
+                    <a href="" class="title"><h4>how to overcome your fears</h4></a>
+                </div>
+                
+                <div class="post clearfix">
+                    <img src="assets/images/20230923_085235.jpg" alt="" >
+                    <a href="" class="title"><h4>how to overcome your fears</h4></a>
+                </div>
+                
+                <div class="post clearfix">
+                    <img src="assets/images/20230717_161310.jpg" alt="">
+                    <a href="" class="title"><h4>how to overcome your fears</h4></a>
+                </div>
+                
+                <div class="post clearfix">
+                    <img src="assets/images/20230923_085235.jpg" alt="">
+                    <a href="" class="title"><h4>how to overcome your fears</h4></a>
+                </div>
+            </div>
+            <div class="section topics">
+                <h2 class="section-title">Topics</h2>
+                <ul>
+                    <li><a href="#">poems</a></li>
+                    <li><a href="#">quotes</a></li>
+                    <li><a href="#">fictions</a></li>
+                    <li><a href="#">biography</a></li>
+                    <li><a href="#">motivation</a></li>
+                    <li><a href="#">inspiration</a></li>
+                    <li><a href="#">life lessons</a></li>
+
+
+                </ul>
+
+            </div>
+        </div>
+        </div>
+    </div>
+    <!--footer-->
+    <?php include(ROOT_PATH."/apps/includes/footer.php"); ?>
+
+    <!--jquery-->
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+   <!--slick carousel-->
+ <script type="text/javascript" src = "https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+   <!-- custom script -->
+   <script src = "assets/js/script.js"></script>
+   </body>
+</html>
