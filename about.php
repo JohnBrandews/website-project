56da19264c762d3762ca7caeb9a374dd7d1d1c5b
john Brandews <johnbrandews@gmail.com>
2024-07-18 23:00:19 +0300

    added file for read more

---
+++ about.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>About</title>
+</head>
+<body>
+<?php include("apps/includes/header.php"); ?>
+
+    
+    <!--page wrapper-->
+    <div class="page-wrapper">
+        <!--content-->
+        <div class="content clearfix">
+            <!--main content-->
+            <div class="main-content single">
+    <h1 class="post-title">This is about our website</h1>
+    <div class="post-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+             Repellat illo praesentium quo debitis recusandae maiores.</p>
+             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam minima alias assumenda placeat maxime ipsum nobis, vel eaque. Velit inventore distinctio voluptates, dolor qui blanditiis eveniet.
+                 Eos cupiditate consectetur quidem.</p>
+                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, quis. Recusandae, labore! Nemo accusamus placeat at officiis provident reprehenderit est quod odio blanditiis, beatae sapiente quo vero, facilis corporis deleniti nobis molestias tenetur illum quidem sit explicabo incidunt. 
+                    Sit, autem?</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                         Doloribus, labore.</p>
+                         
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Repellat illo praesentium quo debitis recusandae maiores.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam minima alias assumenda placeat maxime ipsum nobis, vel eaque. Velit inventore distinctio voluptates, dolor qui blanditiis eveniet.
+                Eos cupiditate consectetur quidem.</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, quis. Recusandae, labore! Nemo accusamus placeat at officiis provident reprehenderit est quod odio blanditiis, beatae sapiente quo vero, facilis corporis deleniti nobis molestias tenetur illum quidem sit explicabo incidunt. 
+                   Sit, autem?</p>
+                   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                        Doloribus, labore.</p>
+                        
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Repellat illo praesentium quo debitis recusandae maiores.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam minima alias assumenda placeat maxime ipsum nobis, vel eaque. Velit inventore distinctio voluptates, dolor qui blanditiis eveniet.
+                Eos cupiditate consectetur quidem.</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, quis. Recusandae, labore! Nemo accusamus placeat at officiis provident reprehenderit est quod odio blanditiis, beatae sapiente quo vero, facilis corporis deleniti nobis molestias tenetur illum quidem sit explicabo incidunt. 
+                   Sit, autem?</p>
+                   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                        Doloribus, labore.</p>
+                        
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Repellat illo praesentium quo debitis recusandae maiores.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam minima alias assumenda placeat maxime ipsum nobis, vel eaque. Velit inventore distinctio voluptates, dolor qui blanditiis eveniet.
+                Eos cupiditate consectetur quidem.</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, quis. Recusandae, labore! Nemo accusamus placeat at officiis provident reprehenderit est quod odio blanditiis, beatae sapiente quo vero, facilis corporis deleniti nobis molestias tenetur illum quidem sit explicabo incidunt. 
+                   Sit, autem?</p>
+                   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                        Doloribus, labore.</p>
+    </div>
+            </div>
+            <div class="sidebar single">
+                <div class="section popular">
+                    <h2 class="section-title">popular</h2>
+                    <div class="post clearfix">
+                        <img src="assets/images/20230923_085235.jpg" alt="" >
+                        <a href="" class="title"><h4>how to overcome your fears</h4></a>
+                    </div>
+                    
+                    <div class="post clearfix">
+                        <img src="assets/images/20230923_085235.jpg" alt="" >
+                        <a href="" class="title"><h4>how to overcome your fears</h4></a>
+                    </div>
+                    
+                    <div class="post clearfix">
+                        <img src="assets/images/20230717_161310.jpg" alt="">
+                        <a href="" class="title"><h4>how to overcome your fears</h4></a>
+                    </div>
+                    
+                    <div class="post clearfix">
+                        <img src="assets/images/20230923_085235.jpg" alt="">
+                        <a href="" class="title"><h4>how to overcome your fears</h4></a>
+                    </div>
+                </div>
+                <div class="section topics">
+                    <h2 class="section-title">Topics</h2>
+                    <ul>
+                        <li><a href="#">poems</a></li>
+                        <li><a href="#">quotes</a></li>
+                        <li><a href="#">fictions</a></li>
+                        <li><a href="#">biography</a></li>
+                        <li><a href="#">motivation</a></li>
+                        <li><a href="#">inspiration</a></li>
+                        <li><a href="#">life lessons</a></li>
+    
+    
+                    </ul>
+    
+                </div>
+            </div>
+            </div>
+        </div>
+        <!--footer-->
+        <div class="footer">
+            <div class="footer-content">
+                <div class="footer-section about">
+                    <h1 class="logo-text"><span>lil</span>inspires</h1>
+                    <p>
+                        lil is known for being the greatest short story writter . 
+                        he usually writes storiess that inspires and changes the lifes of many.
+                        he has now ventured into poem writting.
+                    </p>
+                    <div class="contact">
+                        <span><i class ="fas fa-phone"></i>&nbsp; 254+11166710</span>
+                        <span><i class ="fas fa-envelope"></i>&nbsp; @infolilinspires.com</span>
+                    </div>
+                    <div class="socials">
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-youtube"></i></a>
+    
+                    </div>
+                </div>
+                <div class="footer-section links">
+                <h2>Quick links</h2>
+                <br>
+                <ul>
+                    <a href="#"><li>events</li></a>
+                    <a href="#"><li>Teams</li></a>
+                    <a href="#"><li>mentories</li></a>
+                    <a href="#"><li>gallery</li></a>
+                    <a href="#"><li>terms and conditions</li></a>
+    
+                </ul>
+                </div>
+                <div class="footer-section contact-form">
+                    <h2>contact us</h2>
+                    <br>
+                    <form action="index.html" method="post">
+                        <input type="email" name="email" class="text-input contact-input" placeholder="your email address..">
+                        <textarea rows="4" name="message" class ="text-input contact-input" placeholder="your message.."></textarea>
+                        <button type="submit" class="btn btn-big contact-btn">
+                            <i class="fa fa-envelope"></i>
+                            send
+                        </button>
+                        
+                </div>
+    
+            </div>
+            <div class="footer-bottom">
+                &copy; codingpoets.com | designed by lil bendy
+            </div>
+    
+        </div>
+        
+    <!--jquery-->
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+   <!--slick carousel-->
+ <script type="text/javascript" src = "https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+   <!-- custom script -->
+   <script src = "assets/js/script.js"></script>
+</body>
+</html>
